ab2fcba3d8d8f63a5d3a67b0853e867e36837f4b
Elinton Souza <esc.vistorias@gmail.com>
2026-05-27 19:08:59 -0300

    ✨ Registra rotas de Produtos e Clientes no servidor Express.

--- elinton_store/src/server.ts
+++ elinton_store/src/server.ts
@@ -2,10 +2,16 @@ import express from 'express'
 const app = express()
 const port = 3000
 
+import routesProdutos from "./routes/produtos"
+import routesClientes from "./routes/clientes"
+
 app.use(express.json())
 
+app.use("/produtos", routesProdutos)
+app.use("/clientes", routesClientes)
+
 app.get('/', (req, res) => {
-  res.send('API: Sistema de Controle de Estoque - Loja de Roupas')
+  res.send('API: Sistema de Controle de Estoque - Loja de Roupas do Elinton')
 })
 
 app.listen(port, () => {
